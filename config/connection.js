6c3ba0564c742608360f5684f970b50e49605527
Dylan Best <best.dylan15@gmail.com>
2020-09-17 11:12:57 -0400

    created connection

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// dependencies
+var mysql = require("mysql");
+
+// connect to server
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "135513",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// export connection
+module.exports = connection;
