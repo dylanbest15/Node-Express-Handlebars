4828f79e6868283e3de2bb1d4f9b4b8741621ae5
Dylan Best <best.dylan15@gmail.com>
2020-09-17 22:08:26 -0400

    orm crud functions

--- config/orm.js
+++ config/orm.js
@@ -3,17 +3,41 @@ const connection = require("./connection.js");
 
 // crud query object
 const orm = {
-  selectAll: function() {
-
+  selectAll: function (tableName) {
+    return new Promise((resolve, reject) => {
+      var query = "SELECT * FROM ?";
+      connection.query(query, tableName, function (err, res) {
+        if (err) reject(err);
+        return resolve(res);
+      })
+    })
   },
-  insertOne: function() {
-
+  insertOne: function (tableName, burgerName) {
+    return new Promise((resolve, reject) => {
+      var query = "INSERT INTO ? SET burger_name = ?";
+      connection.query(qyery, [tableName, burgerName], function (err, res) {
+        if (err) reject(err);
+        return resolve(res);
+      })
+    })
   },
-  updateOne: function() {
-
+  updateOne: function (tableName, devoured, burgerName) {
+    return new Promise((resolve, reject) => {
+      var query = "UPDATE ? SET devoured = ? WHERE burger_name = ?";
+      connection.query(query, [tableName, devoured, burgerName], function (err, res) {
+        if (err) reject(err);
+        return resolve(res);
+      })
+    })
   },
-  deleteOne: function() {
-
+  deleteOne: function (tableName, burgerName) {
+    return new Promise((resolve, reject) => {
+      var query = "DELETE FROM ? WHERE burger_name = ?";
+      connection.query(query, [tableName, burgerName], function (err, res) {
+        if (err) reject(err);
+        return resolve(res);
+      })
+    })
   }
 }
 
