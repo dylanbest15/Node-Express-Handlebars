5bef2a09846e8fc9605a9481e05801e0ebc89708
Dylan Best <best.dylan15@gmail.com>
2020-09-17 23:06:43 -0400

    button click events and post requests

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,6 +1,49 @@
 // document ready
-$(function() {
-  // changed devoured with put request
-  // create burger with post request
-  // delete burger with delete request
+$(function () {
+
+  // create burger click event
+  $(".create-form").on("submit", function (event) {
+    event.preventDefault();
+    const newBurger = {
+      burger_name: $("burger-name").val().trim()
+    }
+
+    // create burger post request
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function () {
+      location.reload();
+    })
+  })
+
+  // devour burger click event
+  $(".change-devoured").on("click", function (event) {
+    const id = $(this).data("id");
+    const newDevoured = $(this).data("newdevoured");
+    const newDevouredState = {
+      devoured: newDevoured
+    }
+
+    // update burger put request
+    $.ajax(`/api/burgers/${id}`, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(function () {
+      location.reload();
+    })
+  })
+
+  // delete burger click event
+  $(".delete-burger").on("click", function (event) {
+    const id = $(this).data("id");
+
+    // delete burger delete request
+    $.ajax(`/api/burgers/${id}`, {
+      type: "DELETE"
+    }).then(function() {
+      location.reload();
+    })
+  })
+
 })
