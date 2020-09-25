772d06e924e2c8e6b78103f16e99b6a323599067
Dylan Best <best.dylan15@gmail.com>
2020-09-24 20:31:16 -0400

    connected to jaws db and made env vars

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 // dependencies
 const mysql = require("mysql");
+let connection;
 
 // connect to database
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "135513",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
